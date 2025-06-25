8c00ae630ffdf5da27c00c2f346eb9cfa43d46cf
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-06-25 09:05:07 -0300

    feat: exercício 1 feito com switch

--- ex01.js
+++ ex01.js
@@ -1,10 +1,26 @@
-let a = prompt("Digite o valor de A: ");
-let b = prompt("Digite o valor de B: ");
-let c = prompt("Digite o valor de C: ");
-let delta = (b**2) - (4*a*c)
+let a = parseInt(prompt("Digite o valor de A: "));
+let b = parseInt(prompt("Digite o valor de B: "));
+let c = parseInt(prompt("Digite o valor de C: "));
+// Calcular o discriminante
+let delta = (b**2) - (4*a*c);
+let chave = 0;
+if (delta != 0) {chave = delta > 0 ? 1 : -1}
+// Raízes
+let x1;
+let x2;
 
-if (delta > 0){
-    let x1 = ((-b) + (delta**(1/2)))/(2*a)
-    let x2 = ((-b) - (delta**(1/2)))/(2*a)
-    console.log("Uma raíz vale " + x1 + " a outra vale " + x2)
+
+switch(chave){
+    case 1:
+        x1 = ((-b) + (delta**(1/2)))/(2*a);
+        x2 = ((-b) - (delta**(1/2)))/(2*a);
+        console.log("Possui duas raízes reais distintas valendo " + x1 + " e " + x2)
+        break
+    case 0:
+        x1 = (-b)/(2*a)
+        console.log("Possui raíz dupla valendo " + x1);
+        break
+    case -1:
+        console.log("Não possui raízes reais.");
+        break
 }
