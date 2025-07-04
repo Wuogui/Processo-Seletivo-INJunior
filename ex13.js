c12f94ae5fd5ab98cf51fe7ae22bbe43d653e5b5
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-07-03 22:21:10 -0300

    feat(ex13): função que adiciona jogadores

---
+++ ex13.js
@@ -0,0 +1,14 @@
+
+let time = [];
+function adicionarJogador(nome, idade, posição, pontuação){
+    time.push({
+    Nome: nome,
+    Idade: idade,
+    Posição: posição,
+    Pontuação: pontuação
+})
+}
+
+function buscaPorPosicao(){
+
+}
